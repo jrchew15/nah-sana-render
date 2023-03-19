f6f6f0e687e3de9a7e7756dbcf9f12a9b748f964
Jason Chew <jrchew15@gmail.com>
2023-03-19 09:59:06 -0700

    modified seed commands

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -5,7 +5,7 @@ from .workspaces import seed_workspaces, undo_workspaces
 from .projects import seed_projects, undo_projects
 from .tasks import seed_tasks, undo_tasks
 from .join_tables import seed_join_tables, undo_join_tables
-
+from app.models.db import db, environment, SCHEMA
 
 # Creates a seed group to hold our commands
 # So we can type `flask seed --help`
@@ -15,6 +15,16 @@ seed_commands = AppGroup('seed')
 # Creates the `flask seed all` command
 @seed_commands.command('all')
 def seed():
+    if environment == 'production':
+        # Before seeding, truncate all tables prefixed with schema name
+        db.session.execute(f"TRUNCATE table {SCHEMA}.users RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.workspaces RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.projects RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.tasks RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.user_workspaces RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.user_projects RESTART IDENTITY CASCADE;")
+        # Add a truncate command here for every table that will be seeded.
+        db.session.commit()
     seed_users()
     # Add other seed functions here
     seed_workspaces()
